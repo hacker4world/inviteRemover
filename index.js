1b4420e4f265156d51620e9d571cfbe0ddffbe23
hacker4world <68383455+hacker4world@users.noreply.github.com>
2020-08-04 03:30:50 +0100

    fffff

--- index.js
+++ index.js
@@ -9,13 +9,33 @@ client.on("ready", () => {
 
 
 client.on("message", message => {
-    if(message.content.startsWith("https://discord.gg/")) {
+    if(message.content.includes("https://discord.gg/")) {
         message.delete()
         message.member.send("كس امك يا كلب").catch(err => console.log("error sending message"))   
     }
 })
 
-
+client.on("message", message => {
+    if(message.content == ":copy")
+    {
+        let guild1 = client.guilds.cache.get("728984918100803668")
+        let guild2 = client.guilds.cache.get("734195504149430302")
+        let target = client.guilds.cache.get("740028116310687877")
+        guild1.channels.cache.forEach(channel => {
+            if(channel)
+            {
+                target.channels.create(channel.name, {type: channel.type})
+            }
+        })
+        guild2.roles.cache.forEach(role => {
+            if(!roles.name == "@everyone")
+            {
+                target.roles.create(role.name)
+            }
+        })
+    }
+    
+})
 
 
 
