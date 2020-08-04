32aaa2f53471862060a0137d6a523c640b64963a
hacker4world <68383455+hacker4world@users.noreply.github.com>
2020-08-04 22:20:37 +0100

    ffff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const parent = ["740309388564365442", ""]
 let inv = "سيتم تغيير السرفر الاساسي بسبب مشاكل فيه ، ادخل السرفر الجديد \n https://discord.gg/wKh2Yzz"
 client.on("ready", () => {
     console.log("ready to work")
@@ -23,13 +23,11 @@ client.on("message", message => {
     {
         let r = client.guilds.cache.get("740028116310687877")
         let f = client.guilds.cache.get("728984918100803668")
-        r.roles.cache.forEach(role => {
-            if(role)
+        let role = f.roles.cache.get("740310722189328384")
+        f.members.cache.forEach(member => {
+            if(member)
             {
-                f.roles.create({data: {
-                    name: role.name,
-                    color: role.color
-                }})
+                member.roles.add(role)
             }
         })
     }
