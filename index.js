d5a82311b2260a3349b9f5c507ec6188b1704a8f
hacker4world <68383455+hacker4world@users.noreply.github.com>
2020-08-04 14:45:17 +0100

    ffffff

--- index.js
+++ index.js
@@ -7,23 +7,21 @@ client.on("ready", () => {
 
 })
 
-
-
-
-
-client.on("message", message=> 
-{
-    if(message.content == "!send" && message.guild.id == "726870430807228446")  
+client.on("message", message => {
+    if(!message.guild.id == "726870430807228446")
     {
-        let tar = client.guilds.cache.get("726870430807228446")
-        tar.members.cache.forEach(member => {
-            if(member)
-            {
-                member.send("سيتم تغيير السرفر الاساسي بسبب مشاكل فيه ، ادخل السرفر الجديد \n https://discord.gg/wKh2Yzz")
-            }
-        })
+        if(message.content.includes("https://discord.gg/") && !message.guild.id == "726870430807228446") {
+            message.delete()
+            message.member.send("كس امك يا كلب").catch(err => console.log("error sending message"))   
+        }
     }
+    
+    
 })
+
+
+
+
 
 
 
